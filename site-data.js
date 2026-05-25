1b0b41948cda63608a2f30ff973788ba7831dc2c
Zane V <zane@allied.org>
2026-05-25 16:28:58 -0400

    Make files text funnier (in my opinion at least), fix spelling of frutiger aero, better specify Jibo, edit install mod button, make Open WiFi QR description better, add details on crediting yourself to the site-data.js, expand peoples cards on credits

--- site-data.js
+++ site-data.js
@@ -1,15 +1,81 @@
 const projectProgress = {
-  percentage: 75
+  percentage: 78
 };
 
+// Hello! You're probably here to credit yourself.
+// Let me (ZaneDev) show you how
+// The list below is how you can add yourself
+// First, make sure the person above you has added a comma to their closing bracket (the } symbol)
+// If so then you're ready.
+// First, add your initals. (e.g. Kevin Kor -> kk, ZaneDev -> zv (I used my last name), Jaked -> jk, etc)
+// From here you have a lot of things you can use for describing yourself. I'll list them off
+// 
+// name -> pretty self explanatory. just use a string containing your name
+// initals -> make these your name initals used previously
+// pfp -> link to your profile picture (preferably add it to the assets folder)
+// url -> link to your website (e.g. zane.org, mgdproductions.com, kevinblog.sytes.net, etc.)
+// role -> What you've done in the project. This defaults to "Community Supporter"
+// pro -> Your pronouns :D. Use this to tell people your prefered pronouns (e.g. he/him, she/her, they/them, he/they, she/they, they/he/she, etc (there are basically an infinite amount of pronouns))
+
 const authors = {
-  kk: { name: "Kevin Kor", initials: "KK", pfp: "assets/pfp/kevin.png", url: "https://kevinblog.sytes.net/", role: "Project Lead & Organizer" },
-  jk: { name: "Jaked", initials: "JK", pfp: "assets/pfp/jake.webp", role: "OpenJibo Server Maintainer" },
-  mc: { name: "Marcel", initials: "MC", pfp: "assets/pfp/marcel.webp", url: "https://www.mgdproductions.com/", role: "" },
-  rg: { name: "RobotGuy", initials: "RG", pfp: "assets/pfp/robotguy.webp", role: "" },
-  ww: { name: "Wyatt Woolford", initials: "WW", pfp: "assets/pfp/wyatt.webp", role: "Server Booster" },
-  zm: { name: "Zetoman", initials: "ZM", pfp: "assets/pfp/zetoman.webp", role: "" },
-  ps: { name: "Padre_San", initials: "PS", pfp: "assets/pfp/padre_san.webp", role: "" },
+  kk: {
+    name: "Kevin Kor",
+    initials: "KK",
+    pfp: "assets/pfp/kevin.png",
+    url: "https://kevinblog.sytes.net/",
+    role: "Project Lead & Organizer"
+  },
+  jk: {
+    name: "Jaked",
+    initials: "JK",
+    pfp: "assets/pfp/jake.webp",
+    role: "OpenJibo Server Maintainer"
+  },
+  zv: {
+    name: "ZaneDev",
+    initials: "ZV",
+    pfp: "assets/pfp/zane.jpeg",
+    url: "https://zane.org",
+    pro: "they/he/she",
+    role: "JiboOs Contributor & 5x1 (OpenJibo) Host & etc"
+  },
+  jc: {
+    name: "JackCain",
+    pfp: "assets/pfp/jack.webp",
+    initials: "JC",
+    role: ""
+  },
+  ww: {
+    name: "Wyatt Woolford",
+    initials: "WW",
+    pfp: "assets/pfp/wyatt.webp",
+    role: "Discord Server Booster"
+  },
+  mc: {
+    name: "Marcel",
+    initials: "MC",
+    pfp: "assets/pfp/marcel.webp",
+    url: "https://www.mgdproductions.com/",
+    role: ""
+  },
+  rg: {
+    name: "RobotGuy",
+    initials: "RG",
+    pfp: "assets/pfp/robotguy.webp",
+    role: ""
+  },
+  zm: {
+    name: "Zetoman",
+    initials: "ZM",
+    pfp: "assets/pfp/zetoman.webp",
+    role: ""
+  },
+  ps: {
+    name: "Padre_San",
+    initials: "PS",
+    pfp: "assets/pfp/padre_san.webp",
+    role: ""
+  },
   sd: {
     name: "Alex Rose (aka Jibo Dentist)",
     initials: "AR",
@@ -23,15 +89,6 @@ const authors = {
     pro: "she/they",
     role: "Community Manager & Assist"
   },
-  zv: {
-    name: "ZaneDev",
-    initials: "ZV",
-    pfp: "assets/pfp/zane.jpeg",
-    url: "https://zane.org",
-    pro: "they/he/she",
-    role: "Contributor & 5x1 (OpenJibo) Host"
-  },
-  jc: { name: "JackCain", pfp: "assets/pfp/jack.webp", initials: "JC", role: "" },
 };
 
 const commits = [
